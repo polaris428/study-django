255466e4b353bfcedbb0e889a7a520d696fc9a2e
정민재 <iou1056212@naver.com>
2021-12-01 07:22:43 +0000

    로그인 및 로그아웃

--- pr0909/settings.py
+++ pr0909/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'common.apps.CommonConfig',  
     
     
     'pybo.apps.PyboConfig',
@@ -131,3 +132,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
