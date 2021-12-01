144c34554c5ad2d55f9cecf0df146eb89b3014ec
정민재 <iou1056212@naver.com>
2021-12-01 01:48:45 +0000

    답변 추가 기능 및 예외처리 및 부트스트랩 적용

--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question, Answer
+from .forms import QuestionForm
 
 
 def index(request):
@@ -28,3 +29,20 @@ def answer_create(request, question_id):
     answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
     answer.save()
     return redirect('pybo:detail', question_id=question.id)
+
+
+def question_create(request):
+    """
+    pybo 질문등록
+    """
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'pybo/question_form.html', context)
