8765eaa4b739a2d8ec54b76042df69dfb061ae83
정민재 <iou1056212@naver.com>
2021-12-01 01:54:16 +0000

    답변 등록시  내용이 비어있을시 메세지 출력

--- pybo/forms.py
+++ pybo/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from pybo.models import Question
+from pybo.models import Question, Answer
+
 
 
 class QuestionForm(forms.ModelForm):
@@ -11,3 +12,12 @@ class QuestionForm(forms.ModelForm):
             'subject': '제목',
             'content': '내용',
         }  
+        
+class AnswerForm(forms.ModelForm):
+    class Meta:
+        model = Answer
+        fields = ['content']
+        labels = {
+            'content': '답변내용',
+        }        
+        
